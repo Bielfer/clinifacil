d70fa101321e212d0393f56a2f5a814c894a935f
Gabriel Fernandes <bielfer18@gmail.com>
2022-11-07 00:03:15 -0300

    feat: minimum characters validation message

--- src/constants/validations.ts
+++ src/constants/validations.ts
@@ -3,6 +3,8 @@ const validations = {
   required: 'Campo obrigatório',
   valueGreaterThanZero: 'Insira um valor maior que 0',
   isNumberValid: 'Use ponto em vez de vírgula',
+  minCharacters: (num: number) =>
+    `Esse campo deve conter mais de ${num} caracteres`,
 };
 
 export default validations;
