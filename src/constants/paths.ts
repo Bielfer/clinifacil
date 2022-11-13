99acfabf5eb7678f620adc0ca2fa200051ec0644
Gabriel Fernandes <bielfer18@gmail.com>
2022-11-13 14:46:07 -0300

    feat: sidebar paths array

--- src/constants/paths.ts
+++ src/constants/paths.ts
@@ -1,9 +1,15 @@
+import { BookOpenIcon } from '@heroicons/react/24/outline';
+
 const paths = {
   home: '/',
   pricing: '/#pricing',
   features: '/#features',
   login: '/login',
   records: '/records',
-};
+} as const;
+
+export const sidebarPaths = [
+  { text: 'Prontuário', href: '/records', icon: BookOpenIcon },
+] as const;
 
 export default paths;
