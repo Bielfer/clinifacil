60a80e44a97e92c23423694efdd651951d2858cd
Gabriel Fernandes <bielfer18@gmail.com>
2022-11-15 21:44:52 -0300

    feat: create handbooks api route

---
+++ src/db/handbooks.ts
@@ -0,0 +1,17 @@
+import { adminFirestore, getServerTimestamp } from '@/services/firebase/admin';
+import { Handbook } from '@/types/handbook';
+
+export const setHandbook = (data: Handbook) => {
+  const { id, ...dataWithoutId } = data;
+
+  return adminFirestore
+    .collection('handbooks')
+    .doc(id)
+    .set({
+      ...dataWithoutId,
+      updatedAt: getServerTimestamp(),
+    });
+};
+
+export const deleteHandbook = (handbookId: string) =>
+  adminFirestore.collection('handbooks').doc(handbookId).delete();
---
+++ src/types/handbook.ts
@@ -0,0 +1,39 @@
+import { z } from 'zod';
+
+const handbookFieldOptionSchema = z.object({
+  text: z.string(),
+  value: z.string(),
+});
+
+const handbookFieldTypeSchema = z.enum([
+  'text',
+  'autocomplete',
+  'check',
+  'textarea',
+  'date',
+]);
+
+const handbookFieldSchema = z.object({
+  id: z.string(),
+  label: z.string(),
+  type: handbookFieldTypeSchema,
+  value: z.union([z.string(), z.boolean()]),
+  required: z.boolean().optional(),
+  options: handbookFieldOptionSchema.array().optional(),
+});
+
+export const handbookSchema = z
+  .object({
+    id: z.string(),
+    name: z.string(),
+    fields: handbookFieldSchema.array(),
+  })
+  .strict();
+
+export type Handbook = z.infer<typeof handbookSchema>;
+
+export type HandbookField = z.infer<typeof handbookFieldSchema>;
+
+export type HandbookFieldType = z.infer<typeof handbookFieldTypeSchema>;
+
+export type HandbookFieldOption = z.infer<typeof handbookFieldOptionSchema>;
