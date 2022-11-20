451824d1ed8810eadfc2996b387534cd718533bd
Gabriel Fernandes <bielfer18@gmail.com>
2022-11-19 21:58:14 -0300

    feat: get handbook by id

--- src/db/handbooks.ts
+++ src/db/handbooks.ts
@@ -1,6 +1,9 @@
 import { adminFirestore } from '@/services/firebase/admin';
 import { Handbook } from '@/types/handbook';
 
+export const getHandbookById = (handbookId: string) =>
+  adminFirestore.collection('handbooks').doc(handbookId).get();
+
 export const setHandbook = (data: Handbook) => {
   const { id, ...dataWithoutId } = data;
 
