fd099b1bbbe6f5daa9e5c236fc71b137c72253be
Gabriel Fernandes <bielfer18@gmail.com>
2022-12-29 13:35:12 -0300

    fix: context should have right import

--- src/server/context.ts
+++ src/server/context.ts
@@ -2,10 +2,10 @@ import { type inferAsyncReturnType } from '@trpc/server';
 import { type CreateNextContextOptions } from '@trpc/server/adapters/next';
 import { prisma } from '@/services/prisma';
 import { getServerAuthSession } from '@/helpers/trpc';
-import { ExtendedSession } from '@/types/auth';
+import { Session } from 'next-auth';
 
 type CreateContextOptions = {
-  session: ExtendedSession | null;
+  session: Session | null;
 };
 
 export const createContextInner = async (opts: CreateContextOptions) => ({
