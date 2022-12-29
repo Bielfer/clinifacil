180fefe0f20c404058929276efb203723d18dfae
Gabriel Fernandes <bielfer18@gmail.com>
2022-12-29 01:03:14 -0300

    feat: context for next-auth

--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -1,5 +1,34 @@
-import { initTRPC } from '@trpc/server';
+/* eslint no-console:off */
+import { initTRPC, TRPCError } from '@trpc/server';
+import { Context } from './context';
 
-const t = initTRPC.create();
+const t = initTRPC.context<Context>().create();
 
-export const { router, procedure: publicProcedure } = t;
+export const { router, middleware } = t;
+
+const logger = middleware(async ({ path, type, next }) => {
+  const start = Date.now();
+  const result = await next();
+  const durationMs = Date.now() - start;
+
+  if (result.ok) console.log('OK request timing:', { path, type, durationMs });
+  else console.error('Error request timing', { path, type, durationMs });
+
+  return result;
+});
+
+const isAuthenticated = middleware(({ ctx, next }) => {
+  if (!ctx.session || !ctx.session.user) {
+    throw new TRPCError({ code: 'UNAUTHORIZED' });
+  }
+
+  return next({
+    ctx: {
+      session: { ...ctx.session, user: ctx.session.user },
+    },
+  });
+});
+
+export const publicProcedure = t.procedure.use(logger);
+
+export const privateProcedure = t.procedure.use(logger).use(isAuthenticated);
