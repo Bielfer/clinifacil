211d3e7cb7b4f0ae958468823d1a9c00b9fe72ff
Gabriel Fernandes <bielfer18@gmail.com>
2022-12-29 23:59:22 -0300

    feat: add doctor and receptionist routers

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -1,11 +1,15 @@
 import { publicProcedure, router } from '@/server/trpc';
+import { doctorRouter } from './doctor';
 import { patientRouter } from './patient';
+import { receptionistRouter } from './receptionist';
 import { roleRouter } from './role';
 
 export const appRouter = router({
   healthcheck: publicProcedure.query(() => 'yay!'),
   role: roleRouter,
   patient: patientRouter,
+  receptionist: receptionistRouter,
+  doctor: doctorRouter,
 });
 
 export type AppRouter = typeof appRouter;
