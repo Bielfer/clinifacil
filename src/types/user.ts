69176286177342486062f8e7f55b7df3fff9631b
Gabriel Fernandes <bielfer18@gmail.com>
2022-12-29 23:46:28 -0300

    fix: export user type inside project

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,6 +1,9 @@
 import { User as FirebaseUser } from 'firebase/auth';
+import type { User as NextAuthUser } from 'next-auth';
 import { Role } from './role';
 
 export interface UserAuth extends FirebaseUser {
   roles?: Role[];
 }
+
+export type User = NextAuthUser;
