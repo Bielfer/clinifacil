42906198003368fefae77409732e4b40021aa50c
Gabriel Fernandes <bielfer18@gmail.com>
2023-01-01 12:09:18 -0300

    fix: remove firebase

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import ToastWrapper from '@/components/core/Toast/ToastWrapper';
-import AuthProvider from '@/contexts/auth';
 import { Page } from '@/types/auth';
 import Auth from '@/components/core/Auth';
 import { SessionProvider } from 'next-auth/react';
@@ -14,14 +13,9 @@ interface ExtendedAppProps extends AppProps {
 const MyApp = ({ Component, pageProps }: ExtendedAppProps) => (
   <SessionProvider session={pageProps.session}>
     <ToastWrapper>
-      <AuthProvider>
-        <Auth
-          type={Component.auth}
-          loggedInRedirect={Component.loggedInRedirect}
-        >
-          <Component {...pageProps} />
-        </Auth>
-      </AuthProvider>
+      <Auth type={Component.auth} loggedInRedirect={Component.loggedInRedirect}>
+        <Component {...pageProps} />
+      </Auth>
     </ToastWrapper>
   </SessionProvider>
 );
