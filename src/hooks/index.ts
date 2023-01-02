6505c3c920840230819c36c1bf7a4694d0a480df
Gabriel Fernandes <bielfer18@gmail.com>
2023-01-02 15:48:42 -0300

    fix: export hooks from index file

---
+++ src/hooks/index.ts
@@ -0,0 +1,5 @@
+import useCopyToClipboard from './useCopyToClipboard';
+import useOnClickOutside from './useOnClickOutside';
+import useTimeout from './useTimeout';
+
+export { useCopyToClipboard, useOnClickOutside, useTimeout };
