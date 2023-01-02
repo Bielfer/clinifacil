c6ac3499c280c75686e0165c17e6bf08ab422b3e
Gabriel Fernandes <bielfer18@gmail.com>
2023-01-02 12:14:32 -0300

    feat:queue and patients paths, use them on sidebar

--- src/constants/paths.ts
+++ src/constants/paths.ts
@@ -1,15 +1,18 @@
-import { BookOpenIcon } from '@heroicons/react/24/outline';
+import { BookOpenIcon, UsersIcon } from '@heroicons/react/24/outline';
 
 const paths = {
   home: '/',
   pricing: '/#pricing',
   features: '/#features',
   login: '/login',
-  records: '/records',
+  queue: '/queue',
+  patients: '/patients',
+  patientsById: (id: string) => `/patients/${id}`,
 } as const;
 
 export const sidebarPaths = [
-  { text: 'Prontuário', href: '/records', icon: BookOpenIcon },
+  { text: 'Fila', href: paths.queue, icon: UsersIcon },
+  { text: 'Pacientes', href: paths.patients, icon: BookOpenIcon },
 ] as const;
 
 export default paths;
