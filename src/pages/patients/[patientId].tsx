641ff088a676bd266adb4458a9058285a81427f1
Gabriel Fernandes <bielfer18@gmail.com>
2023-01-03 00:23:49 -0300

    fix: remove unused variables

--- src/pages/patients/[patientId].tsx
+++ src/pages/patients/[patientId].tsx
@@ -3,17 +3,13 @@ import { sidebarPaths } from '@/constants/paths';
 import { Page } from '@/types/auth';
 import Head from 'next/head';
 
-const PatientsById: Page = () => {
-  const t = 't';
-
-  return (
-    <>
-      <Head>
-        <title>CliniFácil | Ver Paciente</title>
-      </Head>
-      <Sidebar items={sidebarPaths}>Test</Sidebar>
-    </>
-  );
-};
+const PatientsById: Page = () => (
+  <>
+    <Head>
+      <title>CliniFácil | Ver Paciente</title>
+    </Head>
+    <Sidebar items={sidebarPaths}>Test</Sidebar>
+  </>
+);
 
 export default PatientsById;
--- src/pages/patients/index.tsx
+++ src/pages/patients/index.tsx
@@ -3,18 +3,15 @@ import { sidebarPaths } from '@/constants/paths';
 import { Page } from '@/types/auth';
 import Head from 'next/head';
 
-const Patients: Page = () => {
-  const t = '';
-  return (
-    <>
-      <Head>
-        <title>CliniFácil | Meus Pacientes</title>
-      </Head>
-      <Sidebar items={sidebarPaths}>
-        <div>temp</div>
-      </Sidebar>
-    </>
-  );
-};
+const Patients: Page = () => (
+  <>
+    <Head>
+      <title>CliniFácil | Meus Pacientes</title>
+    </Head>
+    <Sidebar items={sidebarPaths}>
+      <div>temp</div>
+    </Sidebar>
+  </>
+);
 
 export default Patients;
