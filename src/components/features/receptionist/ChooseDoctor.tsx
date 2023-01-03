a49c9a0404b9d2978f8cc7f3f0f82b296ce52eb1
Gabriel Fernandes <bielfer18@gmail.com>
2023-01-03 00:17:15 -0300

    feat: ChooseDoctor component

---
+++ src/components/features/receptionist/ChooseDoctor.tsx
@@ -0,0 +1,34 @@
+import Dropdown from '@/components/core/Dropdown';
+import { trpc } from '@/services/trpc';
+import useReceptionistStore from '@/store/receptionist';
+import { useSession } from 'next-auth/react';
+
+const ChooseDoctor = () => {
+  const { data: session } = useSession();
+  const userId = session?.user.id;
+  const { data: receptionist } = trpc.receptionist.get.useQuery(
+    {
+      userId,
+    },
+    { enabled: !!userId }
+  );
+  const [selectedDoctorId, setSelectedDoctorId] = useReceptionistStore(
+    (state) => [state.selectedDoctorId, state.setSelectedDoctorId]
+  );
+
+  return (
+    <Dropdown
+      data={
+        receptionist?.doctors.map((doctor) => ({
+          text: doctor.name,
+          value: doctor.id,
+        })) ?? []
+      }
+      setValue={setSelectedDoctorId}
+      value={selectedDoctorId ?? 0}
+      defaultText="Médicos"
+    />
+  );
+};
+
+export default ChooseDoctor;
