a8fa7e2f8e6548a5f27c42b1687f5f7331d1126d
Gabriel Fernandes <bielfer18@gmail.com>
2023-01-03 00:14:54 -0300

    feat: Tabs component should use state

--- src/components/core/Tabs.tsx
+++ src/components/core/Tabs.tsx
@@ -1,62 +1,60 @@
 import { IconType } from '@/types/core';
 import clsx from 'clsx';
-import { useRouter } from 'next/router';
-import MyLink from './MyLink';
+import { Dispatch, SetStateAction } from 'react';
 
-interface Tab {
+interface Tab<TabValueType> {
   text: string;
-  href: string;
+  value: TabValueType;
   icon?: IconType;
 }
 
-interface Props {
-  tabs: Array<Tab>;
+interface Props<ValueType> {
+  tabs: Array<Tab<ValueType>>;
   className?: string;
+  value: ValueType;
+  setValue: Dispatch<SetStateAction<ValueType>>;
 }
 
-const Tabs = ({ tabs, className }: Props) => {
-  const router = useRouter();
+const Tabs = <T,>({ tabs, className, value, setValue }: Props<T>) => (
+  <nav
+    className={clsx(
+      '-mb-px flex max-w-full gap-x-8 overflow-x-auto',
+      className
+    )}
+    aria-label="Tabs"
+  >
+    {tabs.map((tab) => {
+      const isCurrentTab = value === tab.value;
 
-  return (
-    <nav
-      className={clsx(
-        '-mb-px flex gap-x-8 max-w-full overflow-x-auto',
-        className
-      )}
-      aria-label="Tabs"
-    >
-      {tabs.map((tab) => {
-        const isCurrentTab = router.asPath === tab.href;
+      return (
+        <button
+          key={tab.text}
+          type="button"
+          onClick={() => setValue(tab.value)}
+          className={clsx(
+            isCurrentTab
+              ? 'border-primary-500 text-primary-600'
+              : 'border-transparent text-slate-500 hover:border-slate-300 hover:text-slate-700',
+            'group inline-flex items-center whitespace-nowrap border-b-2 py-4 px-1 text-sm font-medium'
+          )}
+        >
+          {tab.icon && (
+            <tab.icon
+              className={clsx(
+                isCurrentTab
+                  ? 'text-primary-500'
+                  : 'text-slate-400 group-hover:text-slate-500',
+                '-ml-0.5 mr-2 h-5 w-5'
+              )}
+              aria-hidden="true"
+            />
+          )}
 
-        return (
-          <MyLink
-            key={tab.href + tab.text}
-            href={tab.href}
-            className={clsx(
-              isCurrentTab
-                ? 'border-primary-500 text-primary-600'
-                : 'border-transparent text-slate-500 hover:text-slate-700 hover:border-slate-300',
-              'group inline-flex items-center py-4 px-1 border-b-2 font-medium text-sm whitespace-nowrap'
-            )}
-          >
-            {tab.icon && (
-              <tab.icon
-                className={clsx(
-                  isCurrentTab
-                    ? 'text-primary-500'
-                    : 'text-slate-400 group-hover:text-slate-500',
-                  '-ml-0.5 mr-2 h-5 w-5'
-                )}
-                aria-hidden="true"
-              />
-            )}
-
-            {tab.text}
-          </MyLink>
-        );
-      })}
-    </nav>
-  );
-};
+          {tab.text}
+        </button>
+      );
+    })}
+  </nav>
+);
 
 export default Tabs;
