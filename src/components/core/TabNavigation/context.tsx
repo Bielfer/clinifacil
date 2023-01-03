78ee1bc518664f8ddbb221a313ccaf02eaaef3b7
Gabriel Fernandes <bielfer18@gmail.com>
2023-01-02 23:29:45 -0300

    fix: remove useless component

--- src/components/core/TabNavigation/context.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useContext } from 'react';
-
-interface ContextType {
-  activeStyles: string;
-}
-
-export const TabNavigationContext = React.createContext({} as ContextType);
-
-export const useTabNavigation = () => useContext(TabNavigationContext);
