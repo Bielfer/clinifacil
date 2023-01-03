e0294c5f591da4d7d7411b19f77ce9970f1c8ec8
Gabriel Fernandes <bielfer18@gmail.com>
2023-01-03 15:30:13 -0300

    fix: return ConditionalWrapper wrapped in fragment

--- src/components/core/ConditionalWrapper/index.tsx
+++ src/components/core/ConditionalWrapper/index.tsx
@@ -1,3 +1,4 @@
+/* eslint react/jsx-no-useless-fragment:off */
 import { ReactNode } from 'react';
 
 interface Props {
@@ -6,7 +7,8 @@ interface Props {
   children: ReactNode;
 }
 
-const ConditionalWrapper = ({ children, condition, renderWrapper }: Props) =>
-  condition ? renderWrapper(children) : children;
+const ConditionalWrapper = ({ children, condition, renderWrapper }: Props) => (
+  <>{condition ? renderWrapper(children) : children}</>
+);
 
 export default ConditionalWrapper;
