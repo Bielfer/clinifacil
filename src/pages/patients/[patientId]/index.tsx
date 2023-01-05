2f97e057043be9625a4bcef91b634339d6e79a5a
Gabriel Fernandes <bielfer18@gmail.com>
2023-01-05 10:59:24 -0300

    fix: migrate to folder structure

---
+++ src/pages/patients/[patientId]/index.tsx
@@ -0,0 +1,33 @@
+import LoadingWrapper from '@/components/core/LoadingWrapper';
+import Sidebar from '@/components/core/Sidebar';
+import Text from '@/components/core/Text';
+import FormPatient from '@/components/forms/FormPatient';
+import { sidebarPaths } from '@/constants/paths';
+import { trpc } from '@/services/trpc';
+import { Page } from '@/types/auth';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
+const PatientsById: Page = () => {
+  const router = useRouter();
+  const patientId = router.query.patientId as string;
+  const { data: patient, isLoading } = trpc.patient.getById.useQuery({
+    id: parseInt(patientId, 10),
+  });
+
+  return (
+    <>
+      <Head>
+        <title>CliniFácil | Detalhes do Paciente</title>
+      </Head>
+      <Sidebar items={sidebarPaths}>
+        <Text h2>Detalhes do Paciente</Text>
+        <LoadingWrapper loading={isLoading}>
+          <FormPatient patient={patient} />
+        </LoadingWrapper>
+      </Sidebar>
+    </>
+  );
+};
+
+export default PatientsById;
