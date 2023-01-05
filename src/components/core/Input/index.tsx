8483893468d991b2832600e118d114a31f13135c
Gabriel Fernandes <bielfer18@gmail.com>
2023-01-05 11:25:06 -0300

    fix: input must have gray background when disabled

--- src/components/core/Input/index.tsx
+++ src/components/core/Input/index.tsx
@@ -18,6 +18,7 @@ const Input: FC<Props> = ({
   error,
   name,
   hint,
+  disabled,
   ...props
 }) => (
   <InputLayout
@@ -31,6 +32,7 @@ const Input: FC<Props> = ({
     <input
       className={clsx(
         'block w-full rounded-lg border',
+        disabled && 'bg-gray-200',
         !error
           ? 'border-gray-300 shadow-sm focus:border-primary-500 focus:ring-primary-500 sm:text-sm'
           : 'border-red-300 pr-10 text-red-900 placeholder-red-300 focus:border-red-500 focus:outline-none focus:ring-red-500 sm:text-sm'
@@ -38,6 +40,7 @@ const Input: FC<Props> = ({
       placeholder={placeholder}
       type={password ? 'password' : type ?? 'text'}
       id={name}
+      disabled={disabled}
       {...props}
     />
   </InputLayout>
