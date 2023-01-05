0db7303a0d14a2e8a7a1a965f0f3310b9758db1e
Gabriel Fernandes <bielfer18@gmail.com>
2023-01-04 21:36:01 -0300

    fix: bug where page fully refreshed on navigation

--- src/components/core/Sidebar/SidebarItem.tsx
+++ src/components/core/Sidebar/SidebarItem.tsx
@@ -1,5 +1,6 @@
 import { SidebarItem as SidebarItemType } from '@/types/sidebar';
 import clsx from 'clsx';
+import Link from 'next/link';
 
 interface Props {
   item: SidebarItemType;
@@ -7,24 +8,24 @@ interface Props {
 }
 
 const SidebarItem = ({ item, current }: Props) => (
-  <a
+  <Link
     href={item.href}
     className={clsx(
       current
         ? 'bg-gray-100 text-gray-900'
         : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900',
-      'group flex items-center px-2 py-2 text-sm font-medium rounded-md'
+      'group flex items-center rounded-md px-2 py-2 text-sm font-medium'
     )}
   >
     <item.icon
       className={clsx(
         current ? 'text-gray-500' : 'text-gray-400 group-hover:text-gray-500',
-        'mr-3 flex-shrink-0 h-6 w-6'
+        'mr-3 h-6 w-6 flex-shrink-0'
       )}
       aria-hidden="true"
     />
     {item.text}
-  </a>
+  </Link>
 );
 
 export default SidebarItem;
