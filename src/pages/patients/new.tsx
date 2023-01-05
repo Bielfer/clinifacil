88c1caa0fd8d0b2fca437780915177dac4cc0644
Gabriel Fernandes <bielfer18@gmail.com>
2023-01-05 01:07:24 -0300

    feat: create patients page

--- src/pages/patients/new.tsx
+++ src/pages/patients/new.tsx
@@ -1,4 +1,6 @@
 import Sidebar from '@/components/core/Sidebar';
+import Text from '@/components/core/Text';
+import PatientForm from '@/components/forms/PatientForm';
 import { sidebarPaths } from '@/constants/paths';
 import { Page } from '@/types/auth';
 import Head from 'next/head';
@@ -8,8 +10,13 @@ const PatientsNew: Page = () => (
     <Head>
       <title>CliniFácil | Nova Consulta</title>
     </Head>
-    <Sidebar items={sidebarPaths}>Temp</Sidebar>
+    <Sidebar items={sidebarPaths}>
+      <Text h2>Crie um novo paciente</Text>
+      <PatientForm />
+    </Sidebar>
   </>
 );
+
+PatientsNew.auth = 'block';
 
 export default PatientsNew;
