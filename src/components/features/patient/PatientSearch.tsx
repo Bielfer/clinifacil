881a3774440eb7597ee9280b2fc008092953e537
Gabriel Fernandes <bielfer18@gmail.com>
2023-01-05 23:39:11 -0300

    feat:link to redirect to patient page, improve responsibility

--- src/components/features/patient/PatientSearch.tsx
+++ src/components/features/patient/PatientSearch.tsx
@@ -128,6 +128,12 @@ const PatientSearch = ({ className }: Props) => {
                         <>
                           <span className="ml-3 flex-auto truncate">
                             {patient.name}
+                            {patient.cpf && (
+                              <span className="md:hidden">
+                                {' - '}
+                                {formatCPF(patient.cpf)}
+                              </span>
+                            )}
                           </span>
                           {active && (
                             <ChevronRightIcon
@@ -145,22 +151,18 @@ const PatientSearch = ({ className }: Props) => {
               {activeOption && (
                 <div className="hidden w-1/2 flex-none flex-col divide-y divide-gray-100 overflow-y-auto sm:flex">
                   <div className="flex flex-auto flex-col justify-between p-6">
-                    <dl className="grid grid-cols-1 gap-x-6 gap-y-3 text-sm text-gray-700">
-                      <dt className="col-end-1 font-semibold text-gray-900">
-                        Nome
-                      </dt>
+                    <dl className="grid grid-cols-2 items-center gap-x-4 gap-y-3 text-sm text-gray-700">
+                      <dt className="font-semibold text-gray-900">Nome</dt>
                       <dd>{activeOption.name}</dd>
                       {activeOption.cpf && (
                         <>
-                          <dt className="col-end-1 font-semibold text-gray-900">
-                            CPF
-                          </dt>
+                          <dt className="font-semibold text-gray-900">CPF</dt>
                           <dd>{formatCPF(activeOption.cpf)}</dd>
                         </>
                       )}
                       {activeOption.birthDate && (
                         <>
-                          <dt className="col-end-1 font-semibold text-gray-900">
+                          <dt className="font-semibold text-gray-900">
                             Data de Nascimento
                           </dt>
                           <dd>
@@ -169,16 +171,23 @@ const PatientSearch = ({ className }: Props) => {
                         </>
                       )}
                     </dl>
-                    <Button
-                      variant="primary"
-                      className="mt-4 justify-center"
-                      loading={isCreatingAppointment}
-                      onClick={() =>
-                        handleButtonCreateAppointment(activeOption.id)
-                      }
-                    >
-                      Colocar na Fila
-                    </Button>
+                    <div className="flex flex-col items-end justify-between gap-y-2 pt-4 lg:flex-row lg:items-center">
+                      <MyLink
+                        href={paths.patientsById(activeOption.id)}
+                        variant="button-secondary"
+                      >
+                        Ver Paciente
+                      </MyLink>
+                      <Button
+                        variant="primary"
+                        loading={isCreatingAppointment}
+                        onClick={() =>
+                          handleButtonCreateAppointment(activeOption.id)
+                        }
+                      >
+                        Colocar na Fila
+                      </Button>
+                    </div>
                   </div>
                 </div>
               )}
