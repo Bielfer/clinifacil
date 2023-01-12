dc1d430529417c4fbad3a5ac1a72818da3b8aa00
Gabriel Fernandes <bielfer18@gmail.com>
2023-01-12 11:23:47 -0300

    fix: add border to links list

--- src/components/core/LinksList/LinksList.tsx
+++ src/components/core/LinksList/LinksList.tsx
@@ -13,7 +13,10 @@ type LinksListType = {
 
 const LinksList: FC<Props> & LinksListType = ({ children, className }) => (
   <div
-    className={clsx('overflow-hidden bg-white shadow sm:rounded-md', className)}
+    className={clsx(
+      'overflow-hidden border bg-white shadow sm:rounded-md',
+      className
+    )}
   >
     <ul className="divide-y divide-gray-200">{children}</ul>
   </div>
