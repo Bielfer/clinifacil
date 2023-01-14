18877f5c9fb7a5eae6986e04fff9734abee3790c
Gabriel Fernandes <bielfer18@gmail.com>
2023-01-14 12:15:13 -0300

    fix: define button or link outside the component

--- src/components/core/EmptyState/EmptyState.tsx
+++ src/components/core/EmptyState/EmptyState.tsx
@@ -1,32 +1,29 @@
 import { IconType } from '@/types/core';
 import clsx from 'clsx';
-import type { FC } from 'react';
-import Button, { ButtonProps } from '../Button';
-import MyLink, { MyLinkProps } from '../MyLink';
+import type { FC, ReactElement } from 'react';
 
 type Props = {
   icon?: IconType;
   title: string;
   subtitle?: string;
-  button?: ButtonProps;
-  link?: MyLinkProps;
+  buttonOrLink?: ReactElement;
 };
 
 const EmptyState: FC<Props> = ({
   icon: Icon,
   title,
   subtitle,
-  button,
-  link,
+  buttonOrLink,
 }) => (
   <div className="text-center">
     {Icon && <Icon className="mx-auto h-12 w-12 text-gray-400" />}
     <h3 className="mt-2 text-sm font-medium text-gray-900">{title}</h3>
-    {subtitle && <p className="mt-1 text-sm text-gray-500">{subtitle}</p>}
-    {button && (
-      <Button {...button} className={clsx('mt-4', button.className)} />
+    {subtitle && (
+      <p className={clsx('mt-1 text-sm text-gray-500', buttonOrLink && 'mb-4')}>
+        {subtitle}
+      </p>
     )}
-    {link && <MyLink {...link} className={clsx('mt-4', link.className)} />}
+    {buttonOrLink}
   </div>
 );
 
