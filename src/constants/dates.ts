1061c10bfc0cc0c4262498b564529699b6f4cebc
Gabriel Fernandes <bielfer18@gmail.com>
2023-01-17 21:23:47 -0300

    feat: day abbreviations and month names

---
+++ src/constants/dates.ts
@@ -0,0 +1,24 @@
+export const dayAbbreviations = [
+  'DOM',
+  'SEG',
+  'TER',
+  'QUA',
+  'QUI',
+  'SEX',
+  'SAB',
+];
+
+export const monthNames = [
+  'Janeiro',
+  'Fevereiro',
+  'Março',
+  'Abril',
+  'Maio',
+  'Junho',
+  'Julho',
+  'Agosto',
+  'Setembro',
+  'Outubro',
+  'Novembro',
+  'Dezembro',
+];
