14b5da6917a6b579c9ced91d57853bfecde0505c
Gabriel Fernandes <bielfer18@gmail.com>
2023-01-20 15:38:55 -0300

    feat: add loading state and change a few texts

--- src/pages/patients/[patientId]/prescriptions/index.tsx
+++ src/pages/patients/[patientId]/prescriptions/index.tsx
@@ -1,6 +1,8 @@
 import Button from '@/components/core/Button';
 import DescriptionList from '@/components/core/DescriptionList';
+import EmptyState from '@/components/core/EmptyState';
 import IconButton from '@/components/core/IconButton';
+import LoadingWrapper from '@/components/core/LoadingWrapper';
 import MyLink from '@/components/core/MyLink';
 import Sidebar from '@/components/core/Sidebar';
 import TabsNavigation from '@/components/core/TabsNavigation';
@@ -15,6 +17,7 @@ import tryCatch from '@/helpers/tryCatch';
 import { trpc } from '@/services/trpc';
 import { Page } from '@/types/auth';
 import { PlusIcon, PrinterIcon, TrashIcon } from '@heroicons/react/20/solid';
+import { DocumentTextIcon } from '@heroicons/react/24/outline';
 import { useSession } from 'next-auth/react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
@@ -25,10 +28,13 @@ const PatientPrescriptions: Page = () => {
   const router = useRouter();
   const patientId = router.query.patientId as string;
   const { addToast } = useToast();
-  const { data: prescriptions, refetch: refetchPrescriptions } =
-    trpc.prescription.getMany.useQuery({
-      patientId: parseInt(patientId, 10),
-    });
+  const {
+    data: prescriptions,
+    isLoading: isLoadingPrescriptions,
+    refetch: refetchPrescriptions,
+  } = trpc.prescription.getMany.useQuery({
+    patientId: parseInt(patientId, 10),
+  });
   const {
     mutateAsync: deletePrescription,
     variables: deleteParameters,
@@ -62,11 +68,11 @@ const PatientPrescriptions: Page = () => {
   return (
     <>
       <Head>
-        <title>CliniFácil | Receitas do Paciente</title>
+        <title>CliniFácil | Receita do Paciente</title>
       </Head>
       <Sidebar items={sidebarPaths}>
         <div className="flex items-center justify-between">
-          <Text h2>Receitas do Paciente</Text>
+          <Text h2>Receita do Paciente</Text>
           <div className="flex flex-col-reverse items-end gap-y-2 pl-3 sm:flex-row sm:items-center sm:gap-y-0 sm:gap-x-3">
             <Button
               iconLeft={PrinterIcon}
@@ -87,7 +93,7 @@ const PatientPrescriptions: Page = () => {
               iconLeft={PlusIcon}
               variant="button-primary"
             >
-              Nova Receita
+              Novo Remédio
             </MyLink>
           </div>
         </div>
@@ -95,42 +101,57 @@ const PatientPrescriptions: Page = () => {
           className="pt-2 pb-6"
           tabs={patientAppointmentPaths({ patientId })}
         />
-        <DescriptionList
-          className="mx-auto max-w-2xl"
-          items={
-            prescriptions?.map((prescription) => ({
-              label: prescription.medicationName,
-              buttonsOrLinks: [
-                <>
-                  <Button
-                    className="hidden sm:inline-flex"
-                    variant="link-error"
-                    iconLeft={TrashIcon}
-                    size="sm"
-                    loading={
-                      isDeletingPrescription &&
-                      deleteParameters?.id === prescription.id
-                    }
-                    onClick={() => handleDeletePrescription(prescription.id)}
-                  >
-                    Apagar
-                  </Button>
-                  <IconButton
-                    className="sm:hidden"
-                    variant="link-error"
-                    icon={TrashIcon}
-                    size="lg"
-                    loading={
-                      isDeletingPrescription &&
-                      deleteParameters?.id === prescription.id
-                    }
-                    onClick={() => handleDeletePrescription(prescription.id)}
-                  />
-                </>,
-              ],
-            })) ?? []
-          }
-        />
+        <LoadingWrapper loading={isLoadingPrescriptions}>
+          {!!prescriptions && prescriptions.length > 0 ? (
+            <DescriptionList
+              title="Remédios"
+              className="mx-auto max-w-2xl pt-5"
+              items={
+                prescriptions?.map((prescription) => ({
+                  label: prescription.medicationName,
+                  buttonsOrLinks: [
+                    <>
+                      <Button
+                        className="hidden sm:inline-flex"
+                        variant="link-error"
+                        iconLeft={TrashIcon}
+                        size="sm"
+                        loading={
+                          isDeletingPrescription &&
+                          deleteParameters?.id === prescription.id
+                        }
+                        onClick={() =>
+                          handleDeletePrescription(prescription.id)
+                        }
+                      >
+                        Apagar
+                      </Button>
+                      <IconButton
+                        className="sm:hidden"
+                        variant="link-error"
+                        icon={TrashIcon}
+                        size="lg"
+                        loading={
+                          isDeletingPrescription &&
+                          deleteParameters?.id === prescription.id
+                        }
+                        onClick={() =>
+                          handleDeletePrescription(prescription.id)
+                        }
+                      />
+                    </>,
+                  ],
+                })) ?? []
+              }
+            />
+          ) : (
+            <EmptyState
+              icon={DocumentTextIcon}
+              title="Nenhum remédio adicionado à receita"
+              subtitle="Para adicionar um novo remédio basta clicar no botão Novo Remédio"
+            />
+          )}
+        </LoadingWrapper>
       </Sidebar>
     </>
   );
