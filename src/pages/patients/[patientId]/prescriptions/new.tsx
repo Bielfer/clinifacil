f5c468d7e030591918f521d3c7a6343547a0a9d6
Gabriel Fernandes <bielfer18@gmail.com>
2023-01-20 13:21:31 -0300

    feat: create prescription page

---
+++ src/pages/patients/[patientId]/prescriptions/new.tsx
@@ -0,0 +1,24 @@
+import Sidebar from '@/components/core/Sidebar';
+import Text from '@/components/core/Text';
+import FormPrescription from '@/components/forms/FormPrescription';
+import { sidebarPaths } from '@/constants/paths';
+import { Page } from '@/types/auth';
+import Head from 'next/head';
+
+const NewPatientPrescription: Page = () => (
+  <>
+    <Head>
+      <title>CliniFácil | Adicione um remédio a Receita</title>
+    </Head>
+    <Sidebar items={sidebarPaths}>
+      <div className="mx-auto max-w-2xl">
+        <Text h2 className="pb-6">
+          Adicione um remédio a Receita
+        </Text>
+        <FormPrescription />
+      </div>
+    </Sidebar>
+  </>
+);
+
+export default NewPatientPrescription;
