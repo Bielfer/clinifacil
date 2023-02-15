0a6a84edba5e9a8bee23387e7ba7e0854f4c7787
Gabriel Fernandes <bielfer18@gmail.com>
2023-02-15 11:26:00 -0300

    fix: appointments query only when doctor exists

--- src/pages/patients/[patientId]/handbooks/new.tsx
+++ src/pages/patients/[patientId]/handbooks/new.tsx
@@ -3,24 +3,24 @@ import Sidebar from '@/components/core/Sidebar';
 import Text from '@/components/core/Text';
 import FormHandbook from '@/components/forms/FormHandbook';
 import { sidebarPaths } from '@/constants/paths';
+import useActiveDoctor from '@/hooks/useActiveDoctor';
 import { trpc } from '@/services/trpc';
 import { Page } from '@/types/auth';
-import { useSession } from 'next-auth/react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 
 const PatientNewHandbook: Page = () => {
   const router = useRouter();
   const patientId = router.query.patientId as string;
-  const { data: session } = useSession();
-  const { data: doctor } = trpc.doctor.get.useQuery({
-    userId: session?.user.id,
-  });
+  const { data: doctor } = useActiveDoctor();
   const { data: appointments, isLoading: isLoadingAppointments } =
-    trpc.appointment.getMany.useQuery({
-      patientId: parseInt(patientId, 10),
-      doctorId: doctor?.id,
-    });
+    trpc.appointment.getMany.useQuery(
+      {
+        patientId: parseInt(patientId, 10),
+        doctorId: doctor?.id,
+      },
+      { enabled: !!doctor }
+    );
   const activeAppointment = appointments?.[0];
 
   return (
