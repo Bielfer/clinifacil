e9ce7e142bd47bc6b8d9a15df8bf22ab70f178c2
Gabriel Fernandes <bielfer18@gmail.com>
2023-02-20 14:55:27 -0300

    fix: add useActiveDoctor to hooks index

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,5 +2,12 @@ import useCopyToClipboard from './useCopyToClipboard';
 import useOnClickOutside from './useOnClickOutside';
 import useTimeout from './useTimeout';
 import useRoles from './useRoles';
+import useActiveDoctor from './useActiveDoctor';
 
-export { useCopyToClipboard, useOnClickOutside, useTimeout, useRoles };
+export {
+  useCopyToClipboard,
+  useOnClickOutside,
+  useTimeout,
+  useRoles,
+  useActiveDoctor,
+};
