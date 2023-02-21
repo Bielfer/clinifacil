6a5e1acb3452cfe961f2d9eb2f23cfc98c65790b
Gabriel Fernandes <bielfer18@gmail.com>
2023-02-21 19:11:32 -0300

    feat: add option to use dash instead of 0

--- src/helpers/formatters.ts
+++ src/helpers/formatters.ts
@@ -1,8 +1,13 @@
-export const formatCurrency = (value: number) =>
-  new Intl.NumberFormat('pt-BR', {
+export const formatCurrency = (
+  value: number,
+  options: { keepZero: boolean } = { keepZero: true }
+) => {
+  if (value === 0 && !options?.keepZero) return ' - ';
+
+  return new Intl.NumberFormat('pt-BR', {
     style: 'currency',
     currency: 'BRL',
   }).format(value / 100);
-
+};
 export const formatCPF = (cpf: string) =>
   cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
