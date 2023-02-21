639afb5453945a65f62d4df4b1eb4ade7d6b0f50
Gabriel Fernandes <bielfer18@gmail.com>
2023-02-21 15:18:21 -0300

    fix: currency should be with cents as integer

--- src/helpers/formatters.ts
+++ src/helpers/formatters.ts
@@ -2,7 +2,7 @@ export const formatCurrency = (value: number) =>
   new Intl.NumberFormat('pt-BR', {
     style: 'currency',
     currency: 'BRL',
-  }).format(value);
+  }).format(value / 100);
 
 export const formatCPF = (cpf: string) =>
   cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
