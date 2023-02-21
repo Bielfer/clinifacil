959c144f8306109af045d32a49b0befaa6f0cb9d
Gabriel Fernandes <bielfer18@gmail.com>
2023-02-21 11:46:23 -0300

    feat: add admin or higher role

--- src/hooks/useRoles.ts
+++ src/hooks/useRoles.ts
@@ -1,12 +1,17 @@
 import { roles } from '@/constants/roles';
+import { isHigherOrEqualInRoleHierarchy } from '@/helpers/roles';
 import { useSession } from 'next-auth/react';
 
 const useRoles = () => {
   const { data: session } = useSession();
   const isDoctor = session?.user.role === roles.doctor;
   const isReceptionist = session?.user.role === roles.receptionist;
+  const isAdminOrHigher = isHigherOrEqualInRoleHierarchy(
+    session?.user.role,
+    roles.admin
+  );
 
-  return { isDoctor, isReceptionist };
+  return { isDoctor, isReceptionist, isAdminOrHigher };
 };
 
 export default useRoles;
