e03fcd79db901ea0b4db84b7bc292b6cee5314a2
Gabriel Fernandes <bielfer18@gmail.com>
2023-02-23 08:51:00 -0300

    feat: add create patient button

--- src/pages/patients/index.tsx
+++ src/pages/patients/index.tsx
@@ -1,7 +1,7 @@
 import Sidebar from '@/components/core/Sidebar';
 import Text from '@/components/core/Text';
 import PatientSearch from '@/components/features/patient/PatientSearch';
-import { sidebarPaths } from '@/constants/paths';
+import paths, { sidebarPaths } from '@/constants/paths';
 import { useRoles } from '@/hooks';
 import useReceptionistStore from '@/store/receptionist';
 import { Page } from '@/types/auth';
@@ -9,6 +9,8 @@ import Head from 'next/head';
 import SelectDoctor from '@/components/features/receptionist/SelectDoctor';
 import { CursorArrowRaysIcon } from '@heroicons/react/24/outline';
 import EmptyState from '@/components/core/EmptyState';
+import MyLink from '@/components/core/MyLink';
+import { PlusIcon } from '@heroicons/react/20/solid';
 
 const Patients: Page = () => {
   const { isReceptionist } = useRoles();
@@ -22,11 +24,21 @@ const Patients: Page = () => {
         <title>CliniFácil | Pacientes</title>
       </Head>
       <Sidebar items={sidebarPaths}>
-        <div className="mx-auto max-w-2xl">
-          <div className="mb-6 flex items-center justify-between">
-            <Text h2>Procure o Paciente</Text>
+        <div className="mb-6 flex items-center justify-between">
+          <Text h2>Procure o Paciente</Text>
+          <div className="flex flex-col-reverse items-end gap-y-2 sm:flex-row sm:items-center sm:gap-y-0 sm:gap-x-4">
             {isReceptionist && <SelectDoctor />}
+            <MyLink
+              href={paths.newPatient}
+              iconLeft={PlusIcon}
+              variant="button-primary"
+            >
+              Novo Paciente
+            </MyLink>
           </div>
+        </div>
+
+        <div className="mx-auto max-w-2xl">
           <div className="flex justify-center pt-6">
             {isReceptionist && !selectedDoctorId ? (
               <EmptyState
