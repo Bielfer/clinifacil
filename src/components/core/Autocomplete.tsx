a4dee8c3ef3bc221621dd8e208e0251d4c4692a4
Gabriel Fernandes <bielfer18@gmail.com>
2023-02-23 13:29:04 -0300

    fix: bug where value was shown, remove duplicate if text is equal

--- src/components/core/Autocomplete.tsx
+++ src/components/core/Autocomplete.tsx
@@ -30,7 +30,9 @@ const Autocomplete: FC<Props> = ({
   placeholder,
   label,
 }) => {
-  const [input, setInput] = useState(selected);
+  const [input, setInput] = useState(
+    options.find((option) => option.value === selected)?.text ?? ''
+  );
 
   const filteredOptions =
     input === ''
@@ -54,12 +56,9 @@ const Autocomplete: FC<Props> = ({
             setInput(event.target.value);
             setSelected(event.target.value);
           }}
-          displayValue={(value: string) => {
-            const t =
-              options.find((option) => option.value === value)?.text ?? input;
-
-            return t;
-          }}
+          displayValue={(value: string) =>
+            options.find((option) => option.value === value)?.text ?? value
+          }
           placeholder={placeholder}
           className={clsx(
             'relative w-full cursor-text rounded-lg border py-2 pl-3 pr-10 text-left focus:outline-none focus-visible:border-primary-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm',
@@ -77,41 +76,43 @@ const Autocomplete: FC<Props> = ({
           />
         </Combobox.Button>
         <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-          {input.length > 0 && (
-            <Combobox.Option
-              value={input}
-              className={({ active }) =>
-                clsx(
-                  'relative cursor-pointer select-none py-2 pl-8 pr-4',
-                  active ? 'bg-primary-600 text-white' : 'text-gray-900'
-                )
-              }
-            >
-              {({ active, selected: selectedOption }) => (
-                <>
-                  <span
-                    className={clsx(
-                      'block truncate',
-                      selectedOption && 'font-semibold'
-                    )}
-                  >
-                    {input}
-                  </span>
-
-                  {selectedOption && (
+          {input.length > 0 &&
+            input !==
+              options.find((option) => option.text === selected)?.text && (
+              <Combobox.Option
+                value={input}
+                className={({ active }) =>
+                  clsx(
+                    'relative cursor-pointer select-none py-2 pl-8 pr-4',
+                    active ? 'bg-primary-600 text-white' : 'text-gray-900'
+                  )
+                }
+              >
+                {({ active, selected: selectedOption }) => (
+                  <>
                     <span
                       className={clsx(
-                        'absolute inset-y-0 left-0 flex items-center pl-1.5',
-                        active ? 'text-white' : 'text-primary-600'
+                        'block truncate',
+                        selectedOption && 'font-semibold'
                       )}
                     >
-                      <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                      {input}
                     </span>
-                  )}
-                </>
-              )}
-            </Combobox.Option>
-          )}
+
+                    {selectedOption && (
+                      <span
+                        className={clsx(
+                          'absolute inset-y-0 left-0 flex items-center pl-1.5',
+                          active ? 'text-white' : 'text-primary-600'
+                        )}
+                      >
+                        <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                      </span>
+                    )}
+                  </>
+                )}
+              </Combobox.Option>
+            )}
           {filteredOptions.map((option) => (
             <Combobox.Option
               key={option.value}
