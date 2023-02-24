bc650571d86018d99bec30e7167a3822437a4c9d
Gabriel Fernandes <bielfer18@gmail.com>
2023-02-24 14:32:39 -0300

    feat: to print field function

--- src/constants/field-types.tsx
+++ src/constants/field-types.tsx
@@ -1,3 +1,6 @@
+/* eslint react/no-array-index-key:off */
+import Table from '@/components/core/Table';
+import Text from '@/components/core/Text';
 import FormikAutocomplete from '@/components/forms/FormikAutocomplete';
 import FormikInput from '@/components/forms/FormikInput';
 import FormikSwitch from '@/components/forms/FormikSwitch';
@@ -49,6 +52,96 @@ export const toRenderField = ({
     ),
     TABLE: <FormikTable name={name} label={label} key={key} />,
   };
+
+  return fieldMatcher[field];
+};
+
+export const toPrintField = ({
+  field,
+  label,
+  value,
+}: {
+  key?: Key | null;
+  field: HandbookFieldType;
+  label?: string | undefined | null;
+  value?: any;
+}) => {
+  const fieldMatcher: Record<HandbookFieldType, JSX.Element | null | boolean> =
+    {
+      TEXT: (
+        <>
+          <Text label className="mb-1">
+            {label}
+          </Text>
+          <Text p>{value ?? ' - '}</Text>
+        </>
+      ),
+      TEXTAREA: (
+        <>
+          <Text label className="mb-1">
+            {label}
+          </Text>
+          <Text p>{value ?? ' - '}</Text>
+        </>
+      ),
+      CHECK: (
+        <>
+          <Text label className="mb-1">
+            {label}
+          </Text>
+          <Text p>{value ?? ' - '}</Text>
+        </>
+      ),
+      AUTOCOMPLETE: (
+        <>
+          <Text label className="mb-1">
+            {label}
+          </Text>
+          <Text p>{value ?? ' - '}</Text>
+        </>
+      ),
+      DATE: (
+        <>
+          <Text label className="mb-1">
+            {label}
+          </Text>
+          <Text p>{value}</Text>
+        </>
+      ),
+      TABLE: Array.isArray(value) && (
+        <>
+          <Text h3 className="mb-2 justify-center">
+            {label}
+          </Text>
+          <Table className="w-full px-2 text-center">
+            <Table.Head>
+              {value?.[0].map((header: string) => (
+                <Table.Header
+                  key={header}
+                  className="border-l border-gray-300 text-center first:border-l-0"
+                >
+                  {header}
+                </Table.Header>
+              ))}
+            </Table.Head>
+            <Table.Body>
+              {value?.slice(1).map((row: string[], idxRow: number) => (
+                <Table.Row key={idxRow}>
+                  {row.map((data, idxCol) => (
+                    <Table.Data
+                      key={idxCol}
+                      className="border-l border-gray-300 first:border-l-0 first:font-semibold"
+                    >
+                      {data}
+                    </Table.Data>
+                  ))}
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </Table>
+        </>
+      ),
+    };
 
   return fieldMatcher[field];
 };
