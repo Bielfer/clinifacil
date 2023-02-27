52bd66b474d4b10d2015bfc6ed0a6028babf77a8
Gabriel Fernandes <bielfer18@gmail.com>
2023-02-27 09:30:38 -0300

    feat: formik add should be autocomplete by default

--- src/components/forms/FormikAdd.tsx
+++ src/components/forms/FormikAdd.tsx
@@ -1,130 +1,73 @@
 import { TrashIcon } from '@heroicons/react/20/solid';
-import { PlusIcon } from '@heroicons/react/24/solid';
-import { FieldArray, useField } from 'formik';
+import { useField } from 'formik';
 import { FC, useState } from 'react';
 import Autocomplete from '../core/Autocomplete';
 import IconButton from '../core/IconButton';
-import Input from '../core/Input';
-import Select from '../core/Select';
 
 type Props = {
   name: string;
   label?: string;
   hint?: string;
   disabled?: boolean;
-} & (
-  | {
-      component: 'input';
-      options?: undefined;
-    }
-  | {
-      component: 'autocomplete';
-      options: { text: string; value: string }[];
-    }
-  | {
-      component: 'select';
-      options: { text: string; value: string }[];
-    }
-);
+  options: { text: string; value: string }[];
+};
 
-const FormikAdd: FC<Props> = ({
-  name,
-  options,
-  label,
-  hint,
-  disabled,
-  component = 'input',
-}) => {
-  const [, { error, touched }] = useField<string[]>(name);
-  const [inputValue, setInputValue] = useState('');
+const FormikAdd: FC<Props> = ({ name, options, label, hint, disabled }) => {
+  const [{ value }, { error, touched, initialValue }, { setValue }] =
+    useField<string[]>(name);
+  const [inputValue, setInputValue] = useState<string[]>(initialValue ?? []);
 
   const errorMessage = (touched && error ? error : '') as string;
 
-  const inputs: Record<typeof component, JSX.Element> = {
-    input: (
-      <Input
-        className="flex-grow"
-        label={label}
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-        name={name}
-        error={errorMessage}
-        hint={hint}
-        disabled={disabled}
-      />
-    ),
-    autocomplete: (
-      <Autocomplete
-        className="flex-grow"
-        label={label}
-        selected={inputValue}
-        setSelected={(e) => setInputValue(e)}
-        options={options ?? []}
-        name={name}
-        error={errorMessage}
-        hint={hint}
-        disabled={disabled}
-      />
-    ),
-    select: (
-      <Select
-        className="flex-grow"
-        label={label}
-        selected={inputValue}
-        setSelected={(e) => setInputValue(e)}
-        options={options ?? []}
-        name={name}
-        error={errorMessage}
-        hint={hint}
-        disabled={disabled}
-      />
-    ),
+  const handleChange = (e: string[]) => {
+    setInputValue(e);
+    setValue(e);
   };
 
   return (
-    <FieldArray name={name}>
-      {({ form, push, remove }) => (
-        <>
-          <div className="flex items-end gap-x-3">
-            {inputs[component]}
-            <IconButton
-              icon={PlusIcon}
-              variant="primary"
-              className="rounded-lg"
-              size="lg"
-              onClick={() => {
-                if (!form.values[name].includes(inputValue)) push(inputValue);
-                setInputValue('');
-              }}
-            />
-          </div>
-          <div className="px-2">
-            {form.values[name].length > 0 && (
-              <div className="mt-2 divide-y divide-gray-200 border-t border-b border-gray-200">
-                {(form.values[name] as string[]).map((insertedValue, idx) => (
-                  <div
-                    className="relative flex items-center py-2"
-                    key={insertedValue}
-                  >
-                    <div className="min-w-0 flex-1 text-sm">
-                      <p className="select-none font-medium text-gray-700">
-                        {insertedValue}
-                      </p>
-                    </div>
+    <>
+      <div className="flex items-end gap-x-3">
+        <Autocomplete
+          key={2}
+          className="flex-grow"
+          label={label}
+          selected={inputValue}
+          setSelected={(e) => handleChange(e as string[])}
+          options={options ?? []}
+          name={name}
+          error={errorMessage}
+          hint={hint}
+          disabled={disabled}
+          multiple
+        />
+      </div>
+      <div className="px-2">
+        {value.length > 0 && (
+          <div className="mt-2 divide-y divide-gray-200 border-t border-b border-gray-200">
+            {value.map((insertedValue) => (
+              <div
+                className="relative flex items-center py-2"
+                key={insertedValue}
+              >
+                <div className="min-w-0 flex-1 text-sm">
+                  <p className="select-none font-medium text-gray-700">
+                    {insertedValue}
+                  </p>
+                </div>
 
-                    <IconButton
-                      icon={TrashIcon}
-                      variant="link-error"
-                      onClick={() => remove(idx)}
-                    />
-                  </div>
-                ))}
+                <IconButton
+                  icon={TrashIcon}
+                  variant="link-error"
+                  onClick={() =>
+                    handleChange(value.filter((item) => item !== insertedValue))
+                  }
+                />
               </div>
-            )}
-          </div>{' '}
-        </>
-      )}
-    </FieldArray>
+            ))}
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
