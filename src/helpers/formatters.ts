211be8d546c7c3e2695f9d4dcd45c7578dca5556
Gabriel Fernandes <bielfer18@gmail.com>
2023-02-28 13:27:10 -0300

    feat: table formatters

--- src/helpers/formatters.ts
+++ src/helpers/formatters.ts
@@ -9,5 +9,37 @@ export const formatCurrency = (
     currency: 'BRL',
   }).format(value / 100);
 };
+
 export const formatCPF = (cpf: string) =>
   cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+
+export const tableFormatters: Record<string, (value: string) => string> = {
+  plusSign: (value: string) => {
+    let formattedString = value;
+
+    if (!value) return value;
+
+    if (!value.includes(',') && !value.includes('.')) formattedString += ',00';
+
+    if (formattedString.includes('-') || formattedString.includes('+'))
+      return formattedString;
+
+    return `+ ${formattedString}`;
+  },
+  degree: (value: string) => {
+    if (!value) return value;
+
+    return value.includes('°') ? value : `${value} °`;
+  },
+  minusSign: (value: string) => {
+    let formattedString = value;
+
+    if (!value) return value;
+
+    if (!value.includes(',') && !value.includes('.')) formattedString += ',00';
+
+    if (formattedString.includes('-')) return formattedString;
+
+    return `- ${formattedString}`;
+  },
+};
