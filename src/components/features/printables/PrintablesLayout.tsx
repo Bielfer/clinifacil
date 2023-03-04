2f0a9af59bc7d8a8bdd697f085ac7c0fd6f137b9
Gabriel Fernandes <bielfer18@gmail.com>
2023-03-04 13:11:48 -0300

    fix: layout should contain name and footer

--- src/components/features/printables/PrintablesLayout.tsx
+++ src/components/features/printables/PrintablesLayout.tsx
@@ -1,21 +1,26 @@
+import Text from '@/components/core/Text';
 import ConditionalWrapper from '@/components/core/ConditionalWrapper';
+import { monthNames } from '@/constants/dates';
 import {
   translateXArray,
   translateXArrayNegative,
   translateYArray,
   translateYArrayNegative,
 } from '@/constants/styles';
-import type { Printable } from '@prisma/client';
+import type { Doctor, Printable } from '@prisma/client';
 import clsx from 'clsx';
+import { getDate, getMonth, getYear } from 'date-fns';
 import { forwardRef, ReactNode } from 'react';
 
 type Props = {
   children: ReactNode;
   printable?: Printable | null;
+  doctor?: Doctor | null;
 };
 
 const PrintablesLayout = forwardRef<HTMLDivElement, Props>(
-  ({ children, printable }, ref) => {
+  ({ children, printable, doctor }, ref) => {
+    const today = new Date();
     const backgroundUrl = printable?.backgroundUrl;
     const translateX = (printable && printable.translateX) ?? 0;
     const translateY = (printable && printable.translateY) ?? 0;
@@ -27,19 +32,21 @@ const PrintablesLayout = forwardRef<HTMLDivElement, Props>(
       translateY >= 0
         ? translateYArray[translateY]
         : translateYArrayNegative[-translateY];
+    const shouldBeCentered = translateX === 0 && translateY === 0;
 
     return (
       <>
         <div
           className={clsx(
             'absolute top-0 left-0 h-full w-full overflow-hidden bg-cover bg-center bg-no-repeat',
-            !backgroundUrl && 'flex items-center justify-center'
+            !(!!backgroundUrl && !shouldBeCentered) &&
+              'flex items-center justify-center'
           )}
           style={{ backgroundImage: `url(${backgroundUrl})` }}
           ref={ref}
         >
           <ConditionalWrapper
-            condition={!!backgroundUrl}
+            condition={!!backgroundUrl && !shouldBeCentered}
             renderWrapper={(toRender) => (
               <div
                 className={clsx(
@@ -52,7 +59,28 @@ const PrintablesLayout = forwardRef<HTMLDivElement, Props>(
               </div>
             )}
           >
-            {children}
+            <div className="flex flex-col items-center">
+              {printable?.displayName && (
+                <Text b className="justify-center">
+                  {printable.name}
+                </Text>
+              )}
+              <div className="my-16 w-full max-w-md px-3">{children}</div>
+              {printable?.footer && (
+                <div>
+                  <Text>
+                    {doctor?.city ?? ''}, {getDate(today)} de{' '}
+                    {monthNames[getMonth(today)]} de {getYear(today)}
+                  </Text>
+                  {doctor && (
+                    <div className="mt-20 flex flex-col items-center border-t border-gray-900">
+                      <Text>Dr. {doctor.name}</Text>
+                      <Text>CRM {doctor.crm}</Text>
+                    </div>
+                  )}
+                </div>
+              )}
+            </div>
           </ConditionalWrapper>
         </div>
         <style type="text/css" media="print">
