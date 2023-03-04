cdad9579dc084b102d29df35776fb9dad91e0965
Gabriel Fernandes <bielfer18@gmail.com>
2023-03-04 12:28:33 -0300

    feat: use printable layout

--- src/components/features/printables/PrescriptionPrintable/PrescriptionPrintable.tsx
+++ src/components/features/printables/PrescriptionPrintable/PrescriptionPrintable.tsx
@@ -1,16 +1,10 @@
 /* eslint react/display-name:off */
 import Text from '@/components/core/Text';
 import { monthNames } from '@/constants/dates';
-import {
-  translateXArray,
-  translateXArrayNegative,
-  translateYArray,
-  translateYArrayNegative,
-} from '@/constants/styles';
 import type { Doctor, Printable, Patient, Prescription } from '@prisma/client';
-import clsx from 'clsx';
 import { getDate, getMonth, getYear } from 'date-fns';
 import { forwardRef } from 'react';
+import PrintablesLayout from '../PrintablesLayout';
 import PrescriptionList from './PrescriptionList';
 
 type Props = {
@@ -23,48 +17,21 @@ type Props = {
 const PrescriptionPrintable = forwardRef<HTMLDivElement, Props>(
   ({ prescriptions, doctor, patient, printable }, ref) => {
     const today = new Date();
-    const doctorPrescriptionBackground = printable?.backgroundUrl;
-    const translateX = (printable && printable.translateX) ?? 0;
-    const translateY = (printable && printable.translateY) ?? 0;
-    const translateXStyle =
-      translateX >= 0
-        ? translateXArray[translateX]
-        : translateXArrayNegative[-translateX];
-    const translateYStyle =
-      translateY >= 0
-        ? translateYArray[translateY]
-        : translateYArrayNegative[-translateY];
 
     return (
-      <div
-        className={clsx(
-          'absolute top-0 left-0 flex h-full w-full items-center justify-center overflow-hidden bg-no-repeat'
-        )}
-        style={{ backgroundImage: `url(${doctorPrescriptionBackground})` }}
-        ref={ref}
-      >
-        {doctorPrescriptionBackground ? (
+      <PrintablesLayout ref={ref} printable={printable}>
+        <div className="flex flex-col items-center">
+          {printable?.displayName && (
+            <Text b className="mb-6 justify-center">
+              {printable.name}
+            </Text>
+          )}
           <PrescriptionList
-            className={clsx(
-              'absolute top-1/2 left-1/2 h-screen w-screen',
-              translateXStyle,
-              translateYStyle
-            )}
+            className="mt-4"
             prescriptions={prescriptions}
             patient={patient}
           />
-        ) : (
-          <div className="flex h-full flex-col items-center justify-between py-40">
-            <Text b className="mb-6 justify-center">
-              Receita
-            </Text>
-            <div>
-              <PrescriptionList
-                className="mt-4"
-                prescriptions={prescriptions}
-                patient={patient}
-              />
-            </div>
+          {printable?.footer && (
             <div>
               <Text>
                 {doctor?.city}, {getDate(today)} de{' '}
@@ -77,9 +44,9 @@ const PrescriptionPrintable = forwardRef<HTMLDivElement, Props>(
                 </div>
               )}
             </div>
-          </div>
-        )}
-      </div>
+          )}
+        </div>
+      </PrintablesLayout>
     );
   }
 );
