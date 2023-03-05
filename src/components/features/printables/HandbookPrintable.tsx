557bd9f8ffe8be47cd569a8d384ffeeb2cbf44b7
Gabriel Fernandes <bielfer18@gmail.com>
2023-03-05 14:38:59 -0300

    fix: use printable layout

--- src/components/features/printables/HandbookPrintable.tsx
+++ src/components/features/printables/HandbookPrintable.tsx
@@ -9,9 +9,8 @@ import type {
 } from '@prisma/client';
 import { forwardRef } from 'react';
 import Text from '@/components/core/Text';
-import { monthNames } from '@/constants/dates';
-import { getDate, getMonth, getYear } from 'date-fns';
 import type { FieldValue } from '@/types/handbook';
+import PrintablesLayout from './PrintablesLayout';
 
 type Props = {
   handbook: Handbook & {
@@ -25,25 +24,14 @@ type Props = {
 const HandbookPrintable = forwardRef<HTMLDivElement, Props>(
   ({ handbook, doctor, patient, printable }, ref) => {
     const toBePrintedFields = handbook.fields.filter((field) => field.print);
-    const today = new Date();
-    const background = printable?.backgroundUrl;
 
     return (
-      <div
-        ref={ref}
-        className="absolute top-0 left-0 flex h-full w-full items-center justify-center overflow-hidden bg-no-repeat"
-        style={{ backgroundImage: `url(${background})` }}
-      >
-        <div className="flex w-1/2 flex-col justify-between">
-          {printable && (
-            <Text h3 className="justify-center">
-              {printable.name}
-            </Text>
-          )}
-          <div>
-            <Text>Para:</Text>
-            <Text className="font-semibold">{patient?.name}</Text>
-          </div>
+      <PrintablesLayout ref={ref} doctor={doctor} printable={printable}>
+        <div>
+          <Text>Para:</Text>
+          <Text b>{patient?.name}</Text>
+        </div>
+        <div className="mt-3 flex flex-col gap-y-3">
           {toBePrintedFields.map((field) => (
             <div key={field.id}>
               {showHandbookField({
@@ -53,20 +41,8 @@ const HandbookPrintable = forwardRef<HTMLDivElement, Props>(
               })}
             </div>
           ))}
-          <div>
-            <Text className="mt-5 justify-center">
-              {doctor?.city}, {getDate(today)} de {monthNames[getMonth(today)]}{' '}
-              de {getYear(today)}
-            </Text>
-            {doctor && (
-              <div className="mt-20 flex flex-col items-center border-t border-gray-900">
-                <Text>Dr. {doctor.name}</Text>
-                <Text>CRM {doctor.crm}</Text>
-              </div>
-            )}
-          </div>
-        </div>{' '}
-      </div>
+        </div>
+      </PrintablesLayout>
     );
   }
 );
