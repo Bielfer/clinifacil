8f11ff1d13c679d4f015d7a94c478adbac413e4a
Gabriel Fernandes <bielfer18@gmail.com>
2023-03-14 08:39:31 -0300

    feat: display children as hidden prop

--- src/components/core/LoadingWrapper.tsx
+++ src/components/core/LoadingWrapper.tsx
@@ -1,16 +1,28 @@
+import clsx from 'clsx';
+import { FC } from 'react';
 import Spinner from './Spinner';
 
 interface Props {
   loading: boolean;
   children: JSX.Element;
+  className?: string;
+  hiddenChildren?: boolean;
 }
 
-const LoadingWrapper = ({ loading, children }: Props) => {
+const LoadingWrapper: FC<Props> = ({
+  loading,
+  children,
+  className,
+  hiddenChildren = false,
+}) => {
   if (loading)
     return (
-      <div className="flex justify-center pt-6">
-        <Spinner size="lg" />
-      </div>
+      <>
+        <div className={clsx('flex justify-center', className)}>
+          <Spinner size="lg" />
+        </div>
+        {hiddenChildren && <div className="hidden">{children}</div>}
+      </>
     );
 
   return children;
