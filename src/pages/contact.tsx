5eed3bb5db6232a57f922992324a5095970392c8
Gabriel Fernandes <bielfer18@gmail.com>
2023-03-14 08:40:00 -0300

    feat: show spinner when iframe is loading

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,24 +1,33 @@
 import Header from '@/components/core/Header';
+import LoadingWrapper from '@/components/core/LoadingWrapper';
 import Footer from '@/components/landing/Footer';
 import { Page } from '@/types/auth';
 import Head from 'next/head';
+import { useState } from 'react';
 
-const Contact: Page = () => (
-  <>
-    <Head>
-      <title>CliniFácil | Fale Conosco</title>
-    </Head>
-    <Header />
-    <main className="flex justify-center py-16">
-      <iframe
-        src="https://tally.so/embed/mRx5r4?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
-        loading="lazy"
-        height="500"
-        title="Fale Conosco"
-      />
-    </main>
-    <Footer />
-  </>
-);
+const Contact: Page = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  return (
+    <>
+      <Head>
+        <title>CliniFácil | Fale Conosco</title>
+      </Head>
+      <Header />
+      <main className="flex flex-col items-center py-16">
+        <LoadingWrapper loading={isLoading} hiddenChildren>
+          <iframe
+            src="https://tally.so/embed/mRx5r4?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
+            loading="lazy"
+            height="500"
+            title="Fale Conosco"
+            onLoad={() => setIsLoading(false)}
+          />
+        </LoadingWrapper>
+      </main>
+      <Footer />
+    </>
+  );
+};
 
 export default Contact;
